d739ad309310babc6f40b7c779b6f75cccf7bfd8
Lauren Dimberg <ldimberg@canallc.com>
2024-06-11 12:46:26 -0500

    Adjusted data file to include portfolio items

--- app/wildkin/page.tsx
+++ app/wildkin/page.tsx
@@ -2,15 +2,30 @@
 "use client";
 import React from "react";
 import ImageGallery from "../_ui/imageGallery";
-import { designTypes } from "../data";
+import { designTypes, wildkin, wildkinPhone } from "../data";
 
-const items = designTypes;
+const items = wildkinPhone;
+const itemsTwo = wildkin;
+
 
 export default function Wildkin() {
-	return (
-		<div className='container flex flex-col items-center h-full pt-10 sm:px-10rem'>
-			<h3 className='mb-4 text-xl py-10'>Wildkin</h3>
-			<ImageGallery items={items} />
-		</div>
-	);
+		return (
+			<>
+				<div className='container flex flex-col items-center h-full pt-10 px-10'>
+					<div className='h-full py-10 text-center md:flex md:justify-between'>
+						<h3 className='text-black font-sans font-semibold text-2xl text-center'>
+							Wildkin
+						</h3>
+						<p className='pt-5 md:w-1/3'>
+							lorem ipsum dolor sit amet, consectetur adipis lorem ipsomething
+							lorem ipsum dolor sit amet, consectetur adipis lorem ipsomething
+							lorem ipsum dolor sit amet, consectetur adipis lorem ipsomething
+						</p>
+					</div>
+					<ImageGallery items={items} />
+					<ImageGallery items={itemsTwo} />
+				</div>
+				;
+			</>
+		);
 }
