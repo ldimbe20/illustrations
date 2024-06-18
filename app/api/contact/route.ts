ba61b34e00ad9156416d65327c6349100988869d
Lauren Dimberg <ldimberg@canallc.com>
2024-06-18 15:55:44 -0500

    Remove hardcoded secrets and use environment variables

---
+++ app/api/contact/route.ts
@@ -0,0 +1,43 @@
+/** @format */
+
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+export async function POST(request: Request) {
+	console.log("Email:", process.env.EMAIL);
+	console.log("Client ID:", process.env.CLIENT);
+	console.log("Client Secret:", process.env.CLIENT_SECRET);
+	console.log("Refresh Token:", process.env.REFRESH_TOKEN);
+	try {
+		const { name, email, message } = await request.json();
+
+		const transporter = nodemailer.createTransport({
+			service: "gmail",
+			secure: true,
+			auth: {
+				type: "OAuth2",
+				user: process.env.EMAIL,
+				clientId: process.env.CLIENT,
+				clientSecret: process.env.CLIENT_SECRET,
+				refreshToken: process.env.REFRESH_TOKEN,
+			},
+		});
+
+		const mailOptions = {
+			from: process.env.EMAIL,
+			to: process.env.EMAIL,
+			subject: "New message from your-website",
+			text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+		};
+
+		await transporter.sendMail(mailOptions);
+
+		return NextResponse.json(
+			{ message: "Message sent successfully" },
+			{ status: 200 }
+		);
+	} catch (error) {
+		console.error("Error sending email:", error);
+		return new NextResponse("Failed to send message.", { status: 500 });
+	}
+}
